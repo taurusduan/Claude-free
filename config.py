775e8954f72f37286af8df70103f16bdf3a63dc8
enzo <huawei@huawei.zone>
2023-07-25 19:25:06 +0800

    add workers.

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ INTERVAL = 2
 SLACK_TOKEN = "xoxp-5608454084855-5616331924118-5635640177057-41f530ba7e4801a17620fc0e6cfc3f65"
 CLAUDE_ID = "U05JARWM1BM"
 CHANNEL_ID = "C05K0LFAGGY"
+
+# Represents how many requests can be processed simultaneously.
+WORKERS = 5
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 
+from config import WORKERS
 from controller.api_router import register_api_routers
 
 app = FastAPI()
 register_api_routers(app)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=WORKERS)
